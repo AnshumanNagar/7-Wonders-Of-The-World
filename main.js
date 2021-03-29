e1e9ad596d2289bdb66168f00d3c8c2dbb99c759
AnshumanNagar <81558720+AnshumanNagar@users.noreply.github.com>
2021-03-29 14:39:40 +0530

    version 1.0

---
+++ main.js
@@ -0,0 +1,43 @@
+var Taj =
+	"Taj Mahal is a monument located in India. It's made of white polished marble and is a great attraction to tourist.";
+
+function tajMahal(){
+	window.alert(Taj);
+} 
+
+var chichen =
+	"Chichén Itzá is a complex of Mayan ruins on Mexico's Yucatán Peninsula. A massive step pyramid, known as El Castillo or Temple of Kukulcan, dominates the ancient city, which thrived from around 600 A.D. to the 1200s. ";
+
+function Chichen(){
+	window.alert(chichen);
+}
+var petra =
+	"Petra is a famous archaeological site in Jordan's southwestern desert. Dating to around 300 B.C., it was the capital of the Nabatean Kingdom. Accessed via a narrow canyon called Al Siq, it contains tombs and temples carved into pink sandstone cliffs, earning its nickname, the Rose City.";
+
+function Petra(){
+	window.alert(petra);
+}
+var machu =
+	"Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned.";
+
+function MP(){
+	window.alert(machu);
+}
+var redeem =
+	"Christ the Redeemer is an Art Deco statue of Jesus Christ in Rio de Janeiro, Brazil, created by Polish-French sculptor Paul Landowski and built by Brazilian engineer Heitor da Silva Costa, in collaboration with French engineer Albert Caquot.";
+
+function Redeemer(){
+	window.alert(redeem);
+}
+var colossem =
+	"The Colosseum, is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum and is the largest ancient amphitheatre ever built, and is still the largest standing amphitheater in the world today, despite its age.";
+
+function colo(){
+	window.alert(colossem);
+}
+var wall =
+	"The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China as protection against various nomadic groups from the Eurasian Steppe.";
+
+function ChinaWall(){
+	window.alert(wall);
+}
